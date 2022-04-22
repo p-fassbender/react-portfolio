a333a6e76f1b7b0f92f267da64c940d52c1e07b7
Preston Fassbender <fassbenderp0551@gmail.com>
2022-04-22 14:51:49 -0500

    pdf viewer iframe for resume component

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import resumePDF from '../../images/Preston-Fassbender-Resume.pdf'
 
 function Resume() {
     return (
-        <section>
+        <section style={{height: "85vh", overflow:"hidden"}}>
             <h1>It's written resume, but it's pronounced resume.</h1>
+            <iframe src={resumePDF} width="100%" height="100%" title='Preston Fassbender Resume'></iframe>
         </section>
     )
 }
