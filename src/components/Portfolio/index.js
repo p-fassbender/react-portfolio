d78fd474486be8873e96f23492e739f17fc5e96c
Preston Fassbender <fassbenderp0551@gmail.com>
2022-04-22 16:57:54 -0500

    array of project objects maps to jsx card element

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Project from '../Project';
 
 function Portfolio() {
+    const [projects] = useState([
+        {
+            title: '3 Guys Pizza-n-Pies',
+            src: require('../../images/3-guys-pizza-n-pies-screenshot.png'),
+            githubLink: 'https://github.com/p-fassbender/3-guys-pizza-n-pies',
+            liveLink: 'https://agile-bastion-05286.herokuapp.com/',
+            technologies: ['HTML5', 'CSS', 'Express', 'MySql', 'Bootstrap', 'Handlebars'],
+            description: 'Small team project for an ecommerce website focused on full stack development following the MVC paradigm'
+        }
+    ])
+
     return (
-        <section>
-            <h1>A Portfolio to show all the things</h1>
-            <Project></Project>
-        </section>
+        <div className='container'>
+            <Project projects={projects}></Project>
+        </div>
     )
 }
 
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function Project() {
+function Project({ projects }) {
+    const [currentProject, setCurrentProject] = useState();
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const toggleModal = (project) => {
+        setCurrentProject({ ...project });
+        setIsModalOpen(!isModalOpen);
+    }
+
     return (
-        <section>
-            <h1>You get a project! You get a project! Everybody here gets a project!</h1>
-        </section>
+        <div>
+            {projects.map((project) => (
+                <div className="card" style={{ width: "18rem" }}>
+                    <img
+                        src={project.src}
+                        alt={project.title}
+                        className=""
+                        key={Math.floor(Math.random() * 100000)}
+                    // onClick={() => toggleModal(project, i)}
+                    />
+                </div>
+            ))}
+        </div>
     )
 }
 
