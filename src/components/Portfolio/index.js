82e51f37613d1d23f6492acdf3a2b6b34365c3e3
Preston Fassbender <fassbenderp0551@gmail.com>
2022-04-22 18:05:32 -0500

    project modal mapping and functionality

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -10,7 +10,15 @@ function Portfolio() {
             liveLink: 'https://agile-bastion-05286.herokuapp.com/',
             technologies: ['HTML5', 'CSS', 'Express', 'MySql', 'Bootstrap', 'Handlebars'],
             description: 'Small team project for an ecommerce website focused on full stack development following the MVC paradigm'
-        }
+        },
+        {
+            title: 'National Parks Info and Weather',
+            src: require('../../images/national-parks-screenshot.png'),
+            githubLink: 'https://github.com/p-fassbender/national-parks-info-and-weather',
+            liveLink: 'https://p-fassbender.github.io/national-parks-info-and-weather',
+            technologies: ['HTML5', 'CSS', 'Foundation', 'JavaScript', ],
+            description: 'Small team project focused on creating an interactive website that integrated data recieved from multiple APIs '
+        },
     ])
 
     return (
--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 
 function Project({ projects }) {
-    const [currentProject, setCurrentProject] = useState();
+    const [currentProject, setCurrentProject] = useState("");
     const [isModalOpen, setIsModalOpen] = useState(false);
     const toggleModal = (project) => {
         setCurrentProject({ ...project });
@@ -10,14 +10,38 @@ function Project({ projects }) {
 
     return (
         <div>
+            <div tabIndex="-1" id="projectModal" className="modal fade" data-bs-backdrop="static" data-bs-keyboard="false" aria-hidden="true">
+                <div className="modal-dialog modal-dialog-centered">
+                    <div className="modal-content">
+                        <div className="modal-header">
+                            <h5 className="modal-title">{currentProject.title}</h5>
+                            <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close" onClick={toggleModal}></button>
+                        </div>
+                        <div className="modal-body">
+                            <img
+                                src={currentProject.src}
+                                alt={currentProject.title}
+                                style={{width: '100%', height: 'auto'}}
+                            />
+                            <p>{currentProject.description}</p>
+                            <p>{currentProject.githubLink}</p>
+                            <p>{currentProject.liveLink}</p>
+                        </div>
+                        <div className="modal-footer">
+                            <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={toggleModal}>Close</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
             {projects.map((project) => (
-                <div className="card" style={{ width: "18rem" }}>
+                <div className="card" style={{ width: "18rem" }} key={Math.floor(Math.random() * 100000)}>
                     <img
                         src={project.src}
                         alt={project.title}
-                        className=""
-                        key={Math.floor(Math.random() * 100000)}
-                    // onClick={() => toggleModal(project, i)}
+                        onClick={() => toggleModal(project)}
+                        data-bs-toggle="modal"
+                        data-bs-target="#projectModal"
                     />
                 </div>
             ))}
